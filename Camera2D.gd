cb2c3d50c458b484fdb41e1c6e404ad3d0415c93
oaparfene <oaparfene@gmail.com>
2020-07-27 10:51:08 +0200

    added button sprites

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 onready var Globals = get_tree().get_root().get_node("Game/Globals")
+onready var vp_dim = get_parent().size 
 
 var target_return_enabled = false
 var target_return_rate = 0.02
@@ -12,7 +13,6 @@ var zoom_speed = 0.05
 var events = {}
 var last_drag_distance = 0
 
-export var CameraSpeed = 25.0
 export var ZoomSpeed = 50.0
 export var ZoomMargin = 0.1
 
@@ -23,6 +23,9 @@ var ZoomPos = Vector2()
 var ZoomFactor = 1.0
 
 
+func _ready():
+	position = vp_dim/2
+	
 func _process(delta):
 	
 		
@@ -47,6 +50,10 @@ func _input(event):
 		events[event.index] = event
 		if events.size() == 1:
 			position -= event.relative * zoom.x
+			if position.x < ((vp_dim[0] / 2) * zoom.x):
+				position.x = ((vp_dim[0] / 2) * zoom.x)
+			if position.y < ((vp_dim[1] / 2) * zoom.y):
+				position.y = ((vp_dim[1] / 2) * zoom.y)
 		elif events.size() == 2:
 			var drag_distance = events[0].position.distance_to(events[1].position)
 			if abs(drag_distance - last_drag_distance) > zoom_sensitivity:
@@ -64,8 +71,8 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_WHEEL_UP:
-				ZoomFactor -= 0.01
+				ZoomFactor -= 0.02
 				ZoomPos = get_global_mouse_position()
 			if event.button_index == BUTTON_WHEEL_DOWN:
-				ZoomFactor += 0.01
+				ZoomFactor += 0.02
 				ZoomPos = get_global_mouse_position()
