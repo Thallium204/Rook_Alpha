34f16c78077b6eaa0b959386a319f763cdc34610
Daniel Jorden <12djorden@gmail.com>
2020-08-28 10:05:19 +0100

    menu tab UI adjustments

--- Scripts/objUpgradesInfo.gd
+++ Scripts/objUpgradesInfo.gd
@@ -19,4 +19,3 @@ func toggleVisible(isCollapsed):
 	visible = isCollapsed
 	twnTabInfo.interpolate_property(self, "rect_size", rect_size, targetTabInfo, 1, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.125)
 	twnTabInfo.start()
-	pass
